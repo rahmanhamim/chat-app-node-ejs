1240180f7031bb7b77915630a941590142877a35
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-01-25 11:55:40 +0600

    initialized

---
+++ app.js
@@ -0,0 +1,53 @@
+// external imports
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+// internal imports
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/common/errorHandler");
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+
+const app = express();
+dotenv.config();
+
+// database connection
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {})
+  .then(() => console.log("database connection successful"))
+  .catch((err) => console.log(err));
+
+// request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listing to port: ${process.env.PORT}`);
+});
