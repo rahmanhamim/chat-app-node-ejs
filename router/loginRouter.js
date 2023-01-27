2712479e849897e80e697b3f91b682f2da9db47b
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-01-27 17:21:25 +0600

    check login on progress

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,15 +2,31 @@
 const express = require("express");
 
 // internal imports
-const { getLogin, login } = require("../controller/loginController");
+const { getLogin, login, logout } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  doLoginValidationHandler,
+  doLoginValidators,
+} = require("../middlewares/login/loginValidator");
 
 const router = express.Router();
 
+// set page title
+const page_title = "Login";
+
 // login page
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), getLogin);
 
 // process login
-router.post("/", login);
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
+
+// logout
+router.delete("/", logout);
 
 module.exports = router;
